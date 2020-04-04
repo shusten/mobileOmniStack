94a5b7eec9007aa3a4ea8d56e4b92ba671eca937
Cov Developer <shusten@hotmail.com>
2020-04-04 17:38:50 -0300

    Hello React

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Hello OmniStack</Text>
     </View>
   );
 }
@@ -12,8 +12,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#7159c1',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  title: {
+    color: '#FFF',
+    fontSize: 30,
+    fontWeight: 'bold'
+  },
+
 });
