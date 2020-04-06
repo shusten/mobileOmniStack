8275ae8f9d9c9d44c03ee75e5f07a89a09d53cd7
Cov Developer <shusten@hotmail.com>
2020-04-05 23:51:47 -0300

    add librarys

--- App.js
+++ App.js
@@ -3,24 +3,8 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello OmniStack</Text>
+    <View>
+      <Text>Hello OmniStack</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#7159c1',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  title: {
-    color: '#FFF',
-    fontSize: 30,
-    fontWeight: 'bold'
-  },
-
-});
---
+++ src/pages/Incidents/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { view } from 'react-native';
+
+export default function Incidents() {
+    return (
+        <view />
+    );
+}
